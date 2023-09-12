d286b53115bb953c16b70a19263cfb558c28f7f9
sheikhfoysaldiu <foysal15-3442@diu.edu.bd>
2023-09-12 19:44:35 +0600

    Fixing Dark Mode

--- src/page/Layout.js
+++ src/page/Layout.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Nav from '../component/nav/Nav'
-import Footer from '../component/footer/Footer'
+import React from "react";
+import Nav from "../component/nav/Nav";
+import Footer from "../component/footer/Footer";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Nav/>
-            {children}
-            <Footer/>
-            </>
-          
-    )
-  }
-  
-  export default Layout
+  return (
+    <div className="bg-indigo-50 dark:bg-zinc-900">
+      <Nav />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
